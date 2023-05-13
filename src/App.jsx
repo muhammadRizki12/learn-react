0884070d98f92c6a86bcbdf63674f8af327404e8
Muhammad Rizki <m.rizkil7621@gmail.com>
2023-05-14 05:45:06 +0700

    Menambahkan tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-    const [count, setCount] = useState(0);
-
+export default function App() {
     return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank' rel='noreferrer'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div
+            className={
+                'bg-blue-600 grid place-content-center min-h-screen text-white font-bold'
+            }
+        >
+            <div className={'text-4xl'}>Learn react</div>
+        </div>
     );
 }
-
-export default App;
