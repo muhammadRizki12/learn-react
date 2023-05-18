68bf0c5c353b19192eae32ac128bb677caad79f6
Muhammad Rizki <m.rizkil7621@gmail.com>
2023-05-19 06:18:05 +0700

    Menambahkan Komponen Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import PlaceContentCenter from './components/placeContentCenter';
-import Button from './components/Button';
-import { useState } from 'react';
+import Counter from './components/Counter';
 
 export default function App() {
-    const [count, setCount] = useState(0);
-
-    function handleClick() {
-        const nextCount = count + 1;
-        setCount((x) => x + 1);
-        console.log({ count, nextCount });
-    }
-
     return (
         <PlaceContentCenter>
-            <h1 className='text-5xl font-bold'>{count}</h1>
-            <div className='mt-5 flex items-center gap-x-2'>
-                <Button onClick={handleClick}>+1</Button>
-                <Button
-                    onClick={() => {
-                        handleClick();
-                        handleClick();
-                        handleClick();
-                    }}>
-                    +3
-                </Button>
-            </div>
+            <Counter initialState={10} />
         </PlaceContentCenter>
     );
 }
